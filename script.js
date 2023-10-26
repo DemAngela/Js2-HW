823bc6819d4815b9dcf34906d9c74e3dbb97cb62
Angela <lika.ponomarenko@mail.ru>
2023-10-26 18:11:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a.charAt(0).toUpperCase() + a.slice(1) + ' ' + b.toLowerCase()
+// console.log(c)
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+//
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.substring(18,21) + ' ' + a.substring(0,18) + a.slice(-4)
+//
+// console.log(b)
+
+// let a = 'children'
+// let b = a.slice(0,-1).toUpperCase() + a.slice(-1).toLowerCase()
+//
+// console.log(b)
+
+// let a = 'boss'
+// let b = a.slice(0,1) + a.slice(-2) + a.slice(1,2)
+//
+// console.log(b)
+
+// let a = 'sunday'
+// let b = a.indexOf('d')
+//
+// console.log(b)
+
+// let a = 'hello'
+// let b = a.replace('ll', 'ks')
+//
+// console.log(b)
+
+// let a = 'user'
+// let b = 'new'
+// let c = b.slice(0) + ' ' + a.slice(0,2)
+//
+// console.log(c)
+
+// let a = 'monday'
+// let b = 'day'
+// let c = a.slice(0,3)
+//
+// console.log(b + b + c)
+//
+// let a = 'HELLO'
+// let b = a[0].toLowerCase() + a[1] + a[2].toLowerCase() + a[3] + a[4].toLowerCase()
+//
+// console.log(b)
+
+// let a = 5
+// let b = '400'
+// let c =  +b.slice(0,2) + a + b.slice(-1)
+// console.log(c)
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a[0] +b[1] + b[2] + b[3] + a[3]
+//
+// console.log(c)
